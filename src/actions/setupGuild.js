ddfaf2159b4944300ebe4ae9ede52c11500579c9
LiTO773 <9774614+LiTO773@users.noreply.github.com>
2019-01-30 15:34:42 +0000

    Removed category

--- src/actions/setupGuild.js
+++ src/actions/setupGuild.js
@@ -3,41 +3,24 @@ import permissionsNeeded from '../helpers/permissions'
 
 // Checks if the bot has all the permissions it needs to function properly
 export default (msg, permissions) => {
-  var missingPermissions = findMissingPermissions(permissions)
-  console.log(missingPermissions.length)
-  if (missingPermissions.length === 0) {
-    msg.channel.send('Sup! Everything looks alright, I\'m ready to rock 🤘')
-    setupRooms(msg)
-  } else {
-    const formatMissing = missingPermissions.reduce((prev, curr) => (
-      `${prev}\n${curr}`
-    ), '```') + '```'
+  if (!states.ready) {
+    var missingPermissions = findMissingPermissions(permissions)
+    console.log(missingPermissions.length)
+    if (missingPermissions.length === 0) {
+      msg.channel.send('Sup! Everything looks alright, I\'m ready to rock 🤘')
+      states.ready = true
+    } else {
+      const formatMissing = missingPermissions.reduce((prev, curr) => (
+        `${prev}\n${curr}`
+      ), '```') + '```'
 
-    msg.channel.send(`Sup! It seems like I have a few permissions missing 😕, please allow me to access this permissions ${formatMissing}`)
+      msg.channel.send(`Sup! It seems like I have a few permissions missing 😕, please allow me to access this permissions ${formatMissing}`)
+    }
+  } else {
+    msg.channel.send('No need for that, it\'s all good 🎉')
   }
 }
 
 const findMissingPermissions = (permissions) => (
   permissionsNeeded.filter(perm => !permissions[perm])
 )
-
-const setupRooms = msg => {
-  // Create the permissions object
-  const roomPermissions = [{
-    id: msg.guild.id,
-    denied: ['READ_MESSAGES', 'SEND_MESSAGES']
-  }, {
-    id: msg.guild.me.id,
-    allowed: ['MANAGE_MESSAGES', 'MANAGE_CHANNELS', 'READ_MESSAGES', 'SEND_MESSAGES']
-  }]
-
-  // Create category for text channels
-  msg.guild.createChannel('Text Rooms', 'category', roomPermissions)
-    .then(channel => {
-      states.category = channel.id
-    })
-    .catch(err => {
-      console.error(err)
-      msg.channel.send('There was an error while setting up, please check the console for more details 😕')
-    })
-}
--- src/states/states.js
+++ src/states/states.js
@@ -1,8 +1,8 @@
 // Rooms are organized by their room id
 // and contain information about their original name, current name and the according text room
-// Category hosts the category where the rooms are created, but will likely be deprecated!
+// Ready stores if the bot is correctly set up
 
 export default {
-  category: '',
+  ready: false,
   rooms: new Map() // What rooms are being used
 }
