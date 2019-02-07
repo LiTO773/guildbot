1a9c60570a998ada66666a269c9559bf6cc82ee4
LiTO773 <devluisp@hotmail.com>
2019-02-07 15:52:13 +0000

    Permission check now doesn't use a message

--- src/actions/setupGuild.js
+++ src/actions/setupGuild.js
@@ -2,22 +2,17 @@ import states from '../states/states'
 import permissionsNeeded from '../helpers/permissions'
 
 // Checks if the bot has all the permissions it needs to function properly
-export default (msg, permissions) => {
-  if (!states.ready) {
-    var missingPermissions = findMissingPermissions(permissions)
-    console.log(missingPermissions.length)
-    if (missingPermissions.length === 0) {
-      msg.channel.send('Sup! Everything looks alright, I\'m ready to rock 🤘')
-      states.ready = true
-    } else {
-      const formatMissing = missingPermissions.reduce((prev, curr) => (
-        `${prev}\n${curr}`
-      ), '```') + '```'
-
-      msg.channel.send(`Sup! It seems like I have a few permissions missing 😕, please allow me to access this permissions ${formatMissing}`)
-    }
+export default (channel, permissions, firstTime) => {
+  var missingPermissions = findMissingPermissions(permissions)
+  if (missingPermissions.length === 0) {
+    if (firstTime) channel.send('Sup! Everything looks alright, I\'m ready to rock 🤘')
+    states.ready = true
   } else {
-    msg.channel.send('No need for that, it\'s all good 🎉')
+    const formatMissing = missingPermissions.reduce((prev, curr) => (
+      `${prev}\n${curr}`
+    ), '```') + '```'
+
+    channel.send(`Sup! It seems like I have a few permissions missing 😕, please allow me to access this permissions ${formatMissing}`)
   }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,20 +1,37 @@
 import { token } from './token'
 import actions from './actions/actions'
 import Discord from 'discord.js'
+import states from './states/states'
 const client = new Discord.Client()
+
+// Permission check
+const defaultCheck = (guild, firstTime) => {
+  const permissions = guild.me.permissions.serialize()
+  const channel = guild.defaultChannel
+  actions.setupGuild(channel, permissions, firstTime)
+}
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`)
+  // Check permissions [SO FAR IT IS ONLY EXPECTING ONE GUILD]
+  client.guilds.forEach(guild => {
+    defaultCheck(guild, true)
+  })
 })
 
+client.on('roleUpdate', (oldR, newR) => defaultCheck(oldR.guild, false))
+client.on('guildMemberUpdate', (oldU, newU) => defaultCheck(oldU.guild, false))
+
 client.on('message', msg => {
+  if (!states.ready) return
   if (msg.content === '!hi') {
     const permissions = msg.guild.me.permissions.serialize()
-    actions.setupGuild(msg, permissions)
+    actions.setupGuild(msg.channel, permissions)
   }
 })
 
 client.on('voiceStateUpdate', (oldMember, newMember) => {
+  if (!states.state) return
   actions.manageRooms(oldMember.voiceChannel, newMember.voiceChannel)
 })
 
